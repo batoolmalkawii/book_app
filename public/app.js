5780e0c3568805f2aef01ef5233bd687ac3bfc0d
Batool Malkawi <batoolmalkawii@gmail.com>
2020-11-04 18:50:41 +0200

    added CSS

--- public/app.js
+++ public/app.js
@@ -4,4 +4,15 @@ $('#showUpdateForm').click(function () {
   $('#updateForm').toggle();
 });
 
+  
+function hide() {
+  $('#list').fadeOut(0);
+  $('#close , #list-item').fadeIn(1000);
+}
+
+function show() {
+  $('#list').fadeIn(1000);
+  $('#close , #list-item').fadeOut(0);
+}
+
 
